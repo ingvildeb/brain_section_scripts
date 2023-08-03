e52e4beefc89eb6135e97fe92c0faaed0d265112
Ingvild Bjerke <i.e.bjerke@medisin.uio.no>
2023-08-03 16:15:26 +0200

    Update graphing_functions.py
    Added two new functions

--- graphing_functions.py
+++ graphing_functions.py
@@ -18,8 +18,12 @@ import xlsxwriter
 from scipy.interpolate import interp1d
 import math
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 
 
+def unique_list(sequence):
+    seen = set()
+    return [x for x in sequence if not (x in seen or seen.add(x))]
 
 
 def get_descriptive_statistics(inputdf, filename, parameter = "None", sheetname = "Sheet1"):
@@ -84,6 +88,71 @@ def create_allen_hbar_graph(filename, input_dir, color_list, region_list, figsiz
     # Create broader groups of regions based on allen hierarchy
     
 
+
+
+## function to create a dict of maximum values for regions within a hierarchical level
+
+
+def create_maxval_dict(customregsfile, superregions_col, regions_col, datafiles):
+    maxval_list = []
+    read_customregs = pd.read_csv(customregsfile, sep = ";")  
+    superregion_list = ((read_customregs[superregions_col]).unique()).tolist()
+    print(superregion_list)
+
+    
+    data = pd.concat(datafiles)
+    for sreg in superregion_list:       
+        regs_in_superreg = (read_customregs[(read_customregs[superregions_col] == sreg)])[regions_col].tolist()
+        flat_list = np.array([data[region].values for region in regs_in_superreg])
+        flat_list[np.isnan(flat_list)] = 0        
+        maxval = flat_list.max()
+
+        
+        maxval_list.append(maxval)
+        maxval_dict = dict(zip(superregion_list, maxval_list))
+    return(maxval_dict)
+    
+
+    
+
+
+
+## function to create line graphs per hierarchical level
+
+def create_line_graphs_per_hierarchy_level(customregsfile, superregions_col, regions_col, maxval_dict, datafile, plot_prefix = ""):
+    
+    read_customregs = pd.read_csv(customregsfile, sep = ";")  
+    superregion_list = ((read_customregs[superregions_col]).unique()).tolist()    
+    
+    for sreg in superregion_list:
+        figure(figsize = (20,10), dpi=80)
+    
+        regs_in_superreg = ((read_customregs[(read_customregs[superregions_col] == sreg)])[regions_col]).tolist()
+        regs_in_superreg = unique_list(regs_in_superreg)
+                
+        ylim = maxval_dict.get(sreg)
+   
+        for region in regs_in_superreg:            
+            data = datafile[region]
+            plt.plot(data, label = region)
+        
+        plt.ylim(0, (ylim + 1000))
+        plt.rc('font', size=10)
+        plt.legend()
+        plt.savefig(plot_prefix + str(sreg) + '.svg', bbox_inches='tight')    
+        plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
 # def complete_regions_list():    
     
 # ids_to_custom = pd.read_excel(r"Y:\Dopamine_receptors\Analysis\resources\ID_to_custom_Newmaster.xlsx")
@@ -94,3 +163,26 @@ def create_allen_hbar_graph(filename, input_dir, color_list, region_list, figsiz
 # df['Custom Region'] = df['Region ID'].map(mydict)
 # df = df[["Section", "Region ID", "Deformation in voxels", "Deformation in um", "Custom Region"]]
 # deformation_per_region = (df.groupby('Custom Region')["Deformation in um"].describe())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
